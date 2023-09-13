9d665f05e12120581ebc3a4934327b81a6d42207
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-09-13 18:05:26 -0300

    componentizando a aplicacao

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps(['tarefas', 'filtroAtual'])
+</script>
+
+<template>
+    <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas" v-if="tarefas.length > 0">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+        <span v-else>Você não possui tarefas {{ props.filtroAtual }}!</span>
+    </ul>
+</template>
+
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
